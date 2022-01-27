b8d3e56eea9d1101cb001ba809c81aefdd839104
Joren Broekema <36734656+undefined@users.noreply.github.com>
2022-01-27 10:23:53 +0000

    refactor: add flexibility, orbiting controls orbiters

--- src/OrbiterElement.ts
+++ src/OrbiterElement.ts
@@ -1,8 +1,24 @@
 import {html, css, LitElement} from 'lit';
-import {customElement} from 'lit/decorators.js';
+import {customElement, property} from 'lit/decorators.js';
+
+const map = (number: number, inMin: number, inMax: number, outMin: number, outMax: number): number => {
+  return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
+
+// Orbiting elements can be customized with these props
+export declare interface CustomOrbitingElement extends HTMLElement {
+  radius: number;
+  angle: number;
+  radian: number;
+  rotationSpeed: number;
+  direction: -1 | 1;
+  orbiting: boolean;
+}
 
 @customElement('orbiter-element')
 export class OrbiterElement extends LitElement {
+  @property({ type: Boolean, reflect: true, attribute: 'start-distributed' }) startDistributed = false;
+
   static styles = css`
     :host {
       display: block;
@@ -22,8 +38,55 @@ export class OrbiterElement extends LitElement {
     return html`
       <div class="orbiter">
         <slot name="content"></slot>
-        <slot name="orbiting"></slot>
+        <slot name="orbiting" @slotchange=${this.handleSlotChange}></slot>
       </div>
     `;
+  }
+
+  handleSlotChange(ev) {
+    const nodes = ev.target.assignedNodes() as CustomOrbitingElement[];
+    nodes.forEach((node, i) => {
+      if (node.orbiting) {
+        return; // node already initialized
+      }
+
+      this.setDefaultsIfUnset(node);
+      // Start them off as distributed along the circle
+      if (this.startDistributed) {
+        node.radian = 360 / nodes.length * i * (Math.PI / 180);
+      }
+      this.setPosition(node);
+      requestAnimationFrame(() => {
+        this.changePosition(node);
+      });
+      node.orbiting = true;
+    })
+  }
+
+  setDefaultsIfUnset(el) {
+    el.radius = el.radius ?? Math.min(map(Math.random(), 0, 1, 120, 180), window.innerWidth / 2 - 75); // Clamp by viewport width;
+    el.angle = el.angle ?? Math.random() * 360;
+    el.rotationSpeed = el.rotationSpeed ?? map(Math.random(), 0, 1, 0.2, 0.3);
+    el.direction = el.direction ?? Math.random() > 0.5 ? 1 : -1;
+
+    el.radian = el.angle * (Math.PI / 180);
+    el.style.position = 'absolute';
+  }
+
+  changePosition(el) {
+    if (!el.matches(':hover')) {
+      el.radian += el.rotationSpeed * (Math.PI / 180) * el.direction;
+      this.setPosition(el);
+    }
+    requestAnimationFrame(() => {
+      this.changePosition(el);
+    });
+  }
+  
+  setPosition(el) {
+    const y = el.radius * Math.sin(el.radian);
+    const x = el.radius * Math.cos(el.radian);
+    el.style.top = `${y}px`;
+    el.style.left = `${x}px`;
   }
 }
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -87,7 +87,7 @@ const svg4 = `
   </svg>
 `;
 
-export const story1 = () => `
+const style = `
   <style>
     .wrap {
       text-align: center;
@@ -98,6 +98,24 @@ export const story1 = () => `
       margin-bottom: 100px;
     }
   </style>
+`;
+
+export const base = () => `
+  ${style}
+  <div class="wrap">
+    <h1>We integrate with lots of tools</h1>
+    <orbiter-element>
+      ${main}
+      <orbiting-element slot="orbiting">${svg1}</orbiting-element>
+      <orbiting-element slot="orbiting">${svg2}</orbiting-element>
+      <orbiting-element slot="orbiting">${svg3}</orbiting-element>
+      <orbiting-element slot="orbiting">${svg4}</orbiting-element>
+    </orbiter-element>
+  </div>
+`;
+
+export const sameDirection = () => `
+  ${style}
   <div class="wrap">
     <h1>We integrate with lots of tools</h1>
     <orbiter-element>
@@ -106,6 +124,76 @@ export const story1 = () => `
       <orbiting-element direction="1" slot="orbiting">${svg2}</orbiting-element>
       <orbiting-element direction="1" slot="orbiting">${svg3}</orbiting-element>
       <orbiting-element direction="1" slot="orbiting">${svg4}</orbiting-element>
+    </orbiter-element>
+  </div>
+`;
+
+export const startDistributed = () => `
+  ${style}
+  <div class="wrap">
+    <h1>Start with orbiting elements distributed</h1>
+    <orbiter-element start-distributed>
+      ${main}
+      <orbiting-element direction="1" slot="orbiting">${svg1}</orbiting-element>
+      <orbiting-element direction="1" slot="orbiting">${svg2}</orbiting-element>
+      <orbiting-element direction="1" slot="orbiting">${svg3}</orbiting-element>
+      <orbiting-element direction="1" slot="orbiting">${svg4}</orbiting-element>
+    </orbiter-element>
+  </div>
+`;
+
+export const speed = () => `
+  ${style}
+  <div class="wrap">
+    <h1>Change the speed of the orbiting elements</h1>
+    <orbiter-element>
+      ${main}
+      <orbiting-element speed="1" slot="orbiting">${svg1}</orbiting-element>
+      <orbiting-element speed="0.1" slot="orbiting">${svg2}</orbiting-element>
+      <orbiting-element speed="0.5" slot="orbiting">${svg3}</orbiting-element>
+      <orbiting-element speed="2" slot="orbiting">${svg4}</orbiting-element>
+    </orbiter-element>
+  </div>
+`;
+
+export const angle = () => `
+  ${style}
+  <div class="wrap">
+    <h1>Change the starting angle of the orbiting elements</h1>
+    <orbiter-element>
+      ${main}
+      <orbiting-element angle="180" slot="orbiting">${svg1}</orbiting-element>
+      <orbiting-element angle="180" slot="orbiting">${svg2}</orbiting-element>
+      <orbiting-element angle="180" slot="orbiting">${svg3}</orbiting-element>
+      <orbiting-element angle="180" slot="orbiting">${svg4}</orbiting-element>
+    </orbiter-element>
+  </div>
+`;
+
+export const floatSpeed = () => `
+  ${style}
+  <div class="wrap">
+    <h1>Change the float speed of the orbiting elements</h1>
+    <orbiter-element>
+      ${main}
+      <orbiting-element float-speed="1" slot="orbiting">${svg1}</orbiting-element>
+      <orbiting-element float-speed="0.1" slot="orbiting">${svg2}</orbiting-element>
+      <orbiting-element float-speed="0.5" slot="orbiting">${svg3}</orbiting-element>
+      <orbiting-element float-speed="2" slot="orbiting">${svg4}</orbiting-element>
+    </orbiter-element>
+  </div>
+`;
+
+export const customElements = () => `
+  ${style}
+  <div class="wrap">
+    <h1>Put in any element you like as orbiting elements</h1>
+    <orbiter-element>
+      ${main}
+      <div style="width: 50px; height: 50px; background-color: lightsalmon" slot="orbiting"></div>
+      <div style="width: 50px; height: 50px; background-color: lightsalmon" slot="orbiting"></div>
+      <div style="width: 50px; height: 50px; background-color: lightsalmon" slot="orbiting"></div>
+      <div style="width: 50px; height: 50px; background-color: lightsalmon" slot="orbiting"></div>
     </orbiter-element>
   </div>
 `;
